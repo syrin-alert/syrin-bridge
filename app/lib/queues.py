a5373d27f68adccb9083b6c193f5753cd4240af4
DiegOps <didevlab@gmail.com>
2025-05-28 06:05:54 +0000

    refactor: ♻ separates functions into files for better maintenance

--- app/lib/queues.py
+++ app/lib/queues.py
@@ -3,5 +3,7 @@ INPUT_QUEUES = ['00_syrin_notification_critical', '00_syrin_notification_warning
 OUTPUT_QUEUES = [
     '01_syrin_notification_audio_process',
     '01_syrin_notification_message_process',
-    '02_syrin_notification_message_process_humanized'
+    '02_syrin_notification_audio_process_category',
+    '02_syrin_notification_message_process_category'
 ]
+01_syrin_notification_message_process
