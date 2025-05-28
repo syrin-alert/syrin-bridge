d8a822f48736272a828752c8132b59ae8ddec46e
Diego Silva <diego.pereira@mutant.com.br>
2025-05-28 16:18:27 -0300

    refactor: ♻ separates functions into files for better maintenance

--- app/lib/publisher.py
+++ app/lib/publisher.py
@@ -1,13 +1,24 @@
+import os
 import pika
 import json
 import logging
+from .queues import OUTPUT_QUEUES, CREATED_QUEUES
 
-from .queues import OUTPUT_QUEUES
+rabbitmq_ttl_dlx = int(os.getenv('RABBITMQ_TTL_DLX', 60000))  # 60 segundos
 
 def ensure_queues_exist(channel):
     for queue in OUTPUT_QUEUES:
         channel.queue_declare(queue=queue, durable=True)
         logging.info(f"Queue '{queue}' checked or created.")
+
+    for queue_pair in CREATED_QUEUES:
+        main_queue, dlx_queue = queue_pair.split(',')
+
+        channel.queue_declare(queue=main_queue, durable=True)
+        logging.info(f"Queue '{main_queue}' checked or created.")
+
+        declare_reprocess_queue(channel, dlx_queue, main_queue)
+        logging.info(f"Queue '{main_queue}' and DLX queue '{dlx_queue}' checked or created.")
 
 def send_to_queues(channel, message):
     try:
@@ -21,3 +32,18 @@ def send_to_queues(channel, message):
             logging.info(f"Message sent to '{queue}': {message}")
     except Exception as e:
         logging.error(f"Error sending message to queues: {str(e)}")
+
+def declare_reprocess_queue(channel, reprocess_queue_name, dead_letter_queue_name):
+    try:
+        channel.queue_declare(
+            queue=reprocess_queue_name,
+            durable=True,
+            arguments={
+                'x-message-ttl': rabbitmq_ttl_dlx,
+                'x-dead-letter-exchange': '',
+                'x-dead-letter-routing-key': dead_letter_queue_name
+            }
+        )
+        logging.info(f"Queue '{reprocess_queue_name}' declared with TTL and DLX.")
+    except Exception as e:
+        logging.error(f"Error declaring reprocess queue '{reprocess_queue_name}': {str(e)}")
--- app/lib/queues.py
+++ app/lib/queues.py
@@ -2,6 +2,10 @@
 INPUT_QUEUES = ['00_syrin_notification_critical', '00_syrin_notification_warning']
 OUTPUT_QUEUES = [
     '01_syrin_notification_audio_process',
-    '01_syrin_notification_message_process',
-    '02_syrin_notification_message_process_humanized'
+    '01_syrin_notification_message_process'
+]
+CREATED_QUEUES = [
+    '02_syrin_notification_audio_process_categories,02_syrin_notification_audio_reprocess_categories',
+    '02_syrin_notification_message_process_categories,02_syrin_notification_message_reprocess_categories',
+    '05_syrin_notification_message_process_humanized,05_syrin_notification_message_reprocess_humanized',
 ]
