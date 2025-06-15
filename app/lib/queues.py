73806a785a1a72b0e8ea0680cc9187e708c6bd8f
DiegOps <didevlab@gmail.com>
2025-06-15 14:50:01 +0000

    refactor: ♻ add queue play

--- app/lib/queues.py
+++ app/lib/queues.py
@@ -7,6 +7,7 @@ OUTPUT_QUEUES = [
 CREATED_QUEUES = [
     '02_syrin_notification_audio_process_categories,02_syrin_notification_audio_reprocess_categories', # Esse processo gerar o texto para ser transformando em TTS
     '03_syrin_notification_audio_process_tts,03_syrin_notification_audio_reprocess_tts', # Esse processso irar transformando o texto em TTS
+    '04_syrin_notification_audio_process_play,03_syrin_notification_audio_reprocess_play', # Esse processso irar tocando o som
 
     '02_syrin_notification_message_process_categories,02_syrin_notification_message_reprocess_categories', # Esse processo gerar o texto para ser enviado ao telegram
     '03_syrin_notification_message_process_send,03_syrin_notification_message_reprocess_send' # Esse processo irar enviando o texto ao telegram
