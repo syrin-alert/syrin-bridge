65e06eb17625dca8e5c3c9701443420262bf1edc
DiegOps <didevlab@gmail.com>
2025-06-15 08:59:10 +0000

    refactor: ♻ remove queue 05_syrin_notification_message_process_categories

--- app/lib/queues.py
+++ app/lib/queues.py
@@ -9,7 +9,5 @@ CREATED_QUEUES = [
     '03_syrin_notification_audio_process_categories,03_syrin_notification_audio_reprocess_categories', # Esse processso irar transformando o texto em TTS
 
     '02_syrin_notification_message_process_categories,02_syrin_notification_message_reprocess_categories', # Esse processo gerar o texto para ser enviado ao telegram
-    '03_syrin_notification_message_process_categories,03_syrin_notification_message_reprocess_categories', # Esse processo irar enviando o texto ao telegram
-
-    '05_syrin_notification_message_process_humanized,05_syrin_notification_message_reprocess_humanized',
+    '03_syrin_notification_message_process_categories,03_syrin_notification_message_reprocess_categories' # Esse processo irar enviando o texto ao telegram
 ]
