71d167ada9b681d39f33652d9676098aa1ec89be
DiegOps <didevlab@gmail.com>
2025-06-15 08:06:04 +0000

    refactor: ♻ create new queue

--- app/lib/queues.py
+++ app/lib/queues.py
@@ -5,7 +5,9 @@ OUTPUT_QUEUES = [
     '01_syrin_notification_message_process'
 ]
 CREATED_QUEUES = [
-    '02_syrin_notification_audio_process_categories,02_syrin_notification_audio_reprocess_categories',
+    '02_syrin_notification_audio_process_categories,02_syrin_notification_audio_reprocess_categories', # Esse processo gerar o texto para ser transformando em TTS
+    '03_syrin_notification_audio_process_categories,03_syrin_notification_audio_reprocess_categories', # Esse processso irar transformando o texto em TTS
+
     '02_syrin_notification_message_process_categories,02_syrin_notification_message_reprocess_categories',
     '05_syrin_notification_message_process_humanized,05_syrin_notification_message_reprocess_humanized',
 ]
